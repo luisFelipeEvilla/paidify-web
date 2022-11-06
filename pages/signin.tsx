0d972f8336a58ce407683c03bd7ff80f8f6cf942
Luis Felipe Evilla <luisevilla588@gmail.com>
2022-11-06 17:51:23 -0500

    set user jwt token on cookies

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -4,12 +4,14 @@ import Image from 'next/image'
 import Link from "next/link";
 import Router from "next/router";
 import { useState } from "react";
+import { useCookies } from "react-cookie";
 import Input from "../components/forms/input";
 
 const Signin: NextPage = () => {
     const [err, setError] = useState(false);
+    const [cookie, setCookie] = useCookies(["token"]);
 
-    const handleOnCahnge = () => {        
+    const handleOnCahnge = () => {
         if (err) setError(false);
     }
 
@@ -23,6 +25,11 @@ const Signin: NextPage = () => {
             user,
             password
         }).then((res) => {
+            setCookie("token", res.data.token, {
+                path: "/",
+                maxAge: 3600, // Expires after 1hr
+                sameSite: true,
+            });
             Router.push('/');
         }).catch((err) => {
             setError(true);
@@ -46,8 +53,8 @@ const Signin: NextPage = () => {
                     <p className="text-sm mt-4">¿Aún no tienes una cuenta? <Link href="/signup"><a className="text-blue-500 text-lg hover:scale-150">Registrate</a></Link></p>
 
                     <div className="flex justify-center	">
-                        <button disabled={err} 
-                        className={`hover:scale-105 ${err ? "bg-gray-500" : "bg-green-500 hover:bg-green-700"} text-white font-bold my-4 py-2 px-4 rounded-lg w-32 `} type="submit">Ingresar</button>
+                        <button disabled={err}
+                            className={`hover:scale-105 ${err ? "bg-gray-500" : "bg-green-500 hover:bg-green-700"} text-white font-bold my-4 py-2 px-4 rounded-lg w-32 `} type="submit">Ingresar</button>
                     </div>
                 </form>
             </div>
